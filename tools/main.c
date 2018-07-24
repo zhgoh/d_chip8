3806b13bd2946f0949d5f7b483407d260f7355c7
zihe.goh <zihe.goh@gmail.com>
2018-07-24 23:55:40 +0800

    Added more test code

--- tools/main.c
+++ tools/main.c
@@ -5,7 +5,14 @@
 #include <stdio.h>
 
 char game[] = {
-    // 0xA1, 0x23   // Set I to 291
+    // Opcode     // Functionality
+    // 0x00, 0xEE,   // Return from subroutine
+    // 0xA1, 0x23    // Set I to 291
+    // 0x21, 0x23,   // Call subroutine at 123
+    // 0x31, 0x00,   // Skips the next instruction if V1 equals 23
+    // 0x41, 0x00,   // Skips the next instruction if V1 not equals 12
+    // 0x51, 0x20,   // Skips the next instruction if V1 equals V2
+    0x61, 0xFA,   // Set V1 to FA
 
 };
 
