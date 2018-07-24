711f60caef1d3ef33a6e3ac6f0132edb1c767280
Goh Zi He <zihe.goh@gmail.com>
2018-07-24 18:07:08 +0800

    Added tools to generate test rom

---
+++ tools/main.c
@@ -0,0 +1,24 @@
+/*
+    Tool to generate a test CHIP-8 rom
+*/
+
+#include <stdio.h>
+
+char game[] = {
+    // 0xA1, 0x23   // Set I to 291
+
+};
+
+int main(void)
+{
+    const size_t sz = sizeof(game);
+
+    FILE *file = fopen("roms/test.ch8", "wb");
+    if (file)
+    {
+        fwrite(game, sizeof(char), sz, file);
+    }
+
+    fclose(file);
+    return 0;
+}
